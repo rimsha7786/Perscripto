eac8e04bbd136b0ec456e0209718c41839b7d535
rimsha7786 <rimshashafqat962@gmail.com>
2026-08-20 17:06:09 +0500

    Update project

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,4 +112,34 @@ else{
     res.json({success:false,message:error.message})
   }
 }
-export { registerUser ,loginUser};
+
+//api to get user profile data
+
+const getProfile = async (req,res) =>{
+
+try{
+const {userId} = req.body
+const userData = await userModel.findById(userId).select('-password')
+
+res.json({success:true,userData})
+
+
+
+}
+catch (error) {
+
+console.log(error)
+res.json({success:false,message:error.message})
+
+
+
+}
+
+
+
+
+
+}
+
+
+export { registerUser ,loginUser,getProfile};
